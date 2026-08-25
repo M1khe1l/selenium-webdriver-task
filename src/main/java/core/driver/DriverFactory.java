33eae17c0aa552825a316888446d917c1cf5efa9
topena <topena00@gmail.com>
2026-08-25 12:43:44 +0400

    Updated (corrected) <Allure.results.directory> path ('mvn clean' now can clean old logs). Added driver.close() before quit() to quitDriver() method.

--- src/main/java/core/driver/DriverFactory.java
+++ src/main/java/core/driver/DriverFactory.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -40,6 +41,7 @@ public class DriverFactory {
         if (driver != null) {
             logger.info("Quitting WebDriver instance for thread: {}", Thread.currentThread().getName());
             try{
+                driver.close();
                 driver.quit();
             } catch (Exception e){
                 logger.error("Error quitting WebDriver instance", e);
@@ -52,7 +54,7 @@ public class DriverFactory {
     }
 
     private static WebDriver createDriver(BrowserType browserType) {
-        WebDriver driver;
+        RemoteWebDriver driver;
         switch (browserType) {
             case FIREFOX -> {
                 logger.info("Creating Firefox driver");
