6e3a11efa60dcddc23fbe3e3df5cefe781c38b29
topena <topena00@gmail.com>
2026-09-01 23:21:01 +0400

    Added ReportPortal integration for test reporting

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,5 +1,9 @@
 package base;
 
+import com.epam.reportportal.message.ReportPortalMessage;
+import com.epam.reportportal.service.ReportPortal;
+import com.epam.reportportal.testng.ReportPortalTestNGListener;
+import com.epam.reportportal.utils.files.ByteSource;
 import core.config.ConfigReader;
 import core.driver.DriverFactory;
 import core.reporting.ScreenshotUtils;
@@ -11,7 +15,9 @@ import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Listeners;
 
+@Listeners(ReportPortalTestNGListener.class)
 public class BaseTest {
 
     protected static final Logger log = LoggerFactory.getLogger(BaseTest.class);
@@ -32,7 +38,13 @@ public class BaseTest {
             log.info("Test Failed: {}", result.getName());
             byte[] screenshot = takeScreenshot();
             ScreenshotUtils.saveToDisk(screenshot, result.getName());
+            ReportPortal.emitLog(
+                    new ReportPortalMessage(ByteSource.wrap(screenshot), "image/png", "Screenshot on failure"),
+                    "ERROR",
+                    java.util.Calendar.getInstance().getTime()
+            );
         }
+
     }
 
     @AfterClass(alwaysRun = true)
